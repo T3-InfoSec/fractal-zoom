d1848bb1dac3b7b4f0d795f46ade12bd2c2eaecf
masterchief1604 <masterchief164@proton.me>
2024-11-27 21:42:42 +0530

    feat: added support for communication with flutter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import BurningShip from "./Components/BurningShip/BurningShip.tsx";
 import Controls from "./Components/Controls/Controls.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 function App() {
     const [position, setPosition] = useState({x: 0, y: 0});
     const [reset, setReset] = useState(false);
@@ -12,6 +12,30 @@ function App() {
     const updatePosition = (position: {x: number, y: number}) => {
         setPosition(position);
     }
+
+    useEffect(() => {
+        window._flutterMessages = [];
+
+        window.sendToFlutter = function(message: string) {
+            window._flutterMessages.push(JSON.stringify(message));
+            console.log('Message added to buffer:', message);
+        };
+
+        window.getFlutterMessages = function() {
+            const messages = window._flutterMessages;
+            window._flutterMessages = [];
+            return JSON.stringify(messages);
+        };
+
+        window.receiveFromFlutter = function(message: string) {
+            const event = new CustomEvent('flutterMessage', {
+                detail: JSON.parse(message)
+            });
+            window.dispatchEvent(event);
+            console.log('Received from Flutter:', message);
+        };
+    }, []);
+
   return (
     <>
         <BurningShip updatePosition={updatePosition}
